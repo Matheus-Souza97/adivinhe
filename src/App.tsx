e87b59d022fb8c097c44f2207d6a9bbe871eb1d3
Matheus-Souza97 <matheus-souza1@outlook.com.br>
2026-07-09 17:29:41 -0300

    create button component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letters } from "./components/Letters"
 import { Input } from "./components/Input"
+import { Button } from "./components/Button"
 import styles from "./app.module.css"
 
 export default function App() {
@@ -26,10 +27,9 @@ export default function App() {
 
       <h4>Palpite</h4>
 
-      <div>
-
+      <div className= {styles.guess}>
         <Input autoFocus maxLength={1} placeholder="?"/>
-
+        <Button title="Confirmar"/>
       </div>
     </main>
 
