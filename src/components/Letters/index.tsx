470304cf51d32c2bc44dc3eb96c826c9c339055a
Matheus-Souza97 <matheus-souza1@outlook.com.br>
2026-07-10 10:01:14 -0300

    add text color variants"

--- src/components/Letters/index.tsx
+++ src/components/Letters/index.tsx
@@ -2,12 +2,14 @@ import styles from "./styles.module.css"
 
 type Props = {
   value?: string
+  size?: "default" | "small"
+  color?: "default" | "correct" | "wrong"
 }
 
 
-export function Letters({value = ""}: Props){
+export function Letters({value = "", size = "default", color = "default"}: Props){
   return(
-    <div className={styles.letters}>
+    <div className={`${styles.letters} ${size === "small" && styles.lettersSmall} ${color === "correct" && styles.lettersCorrect} ${color === "wrong" && styles.lettersWrong}`}>
       <span>{value}</span>
     </div>
   )
--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,9 +1,18 @@
 import styles from "./styles.module.css"
+import { Letters } from "../Letters"
 
 export function LettersUser(){
   return (
     <div className= {styles.lettersUsed}>
       <h5>Letras Utilizadas</h5>
+
+      <div>
+        <Letters value="R" size="small" color="correct"/>
+        <Letters value="E" size="small" color="wrong"/>
+        <Letters value="A" size="small"/>
+        <Letters value="C" size="small"/>
+        <Letters value="T" size="small"/>
+      </div>
     </div>
   )
 }
